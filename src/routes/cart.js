ff5512b8042599053fa1b17586c6c72c07983c03
Shainkey Asati <52102782+shainkey98@users.noreply.github.com>
2021-05-24 09:25:20 +0530

    Update cart.js
    post >> get

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -8,7 +8,7 @@ const {
 const { requireSignin, userMiddleware } = require("../common-middleware");
 const router = express.Router();
 
-router.post(
+router.get(
   "/user/cart/addtocart",
   requireSignin,
   userMiddleware,
